26f70e1817f776d36a2cdaaaed5eb6c55552f291
nyh365 <12nyk34@naver.com>
2023-03-14 10:22:04 +0900

    [S08P22A308-46] BE/Fix: edit user entity

--- backend/src/main/java/com/ssafy/mongttang/entity/User.java
+++ backend/src/main/java/com/ssafy/mongttang/entity/User.java
@@ -1,7 +1,11 @@
 package com.ssafy.mongttang.entity;
 
 import com.ssafy.mongttang.dto.AuthProvider;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
 import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.*;
@@ -10,8 +14,11 @@ import java.time.LocalDateTime;
 
 @Getter
 @Entity
+@DynamicInsert
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "user")
-public class User {
+public class User extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -39,6 +46,17 @@ public class User {
 
     @Column
     private String userInfo;
+
+    @Builder
+    public User(int userId, LocalDateTime userCreateDate, String userEmail, String userNickname,String userGender, AuthProvider userProvider ){
+        this.userId = userId;
+        this.userCreateDate = userCreateDate;
+        this.userEmail = userEmail;
+        this.userNickname = userNickname;
+        this.userGender = userGender;
+        this.userRole = "ROLE_USER";
+        this.userProvider = userProvider;
+    }
 
     public void changeNickname(String nickname) {
         this.userNickname = nickname;
