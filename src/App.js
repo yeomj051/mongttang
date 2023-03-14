55af3accb03f9fdd8b63c0c69afc467dd989e564
dayong2eyo <109324544+dayong2eyo@users.noreply.github.com>
2023-03-14 15:33:40 +0900

    [S08P22A308-102] FE/feat: setting

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import { Route, Routes, BrowserRouter, Navigate } from 'react-router-dom';
 
+// component
+import Home from 'pages/Home/Home';
+// react-cookie
+import { CookiesProvider } from 'react-cookie';
+// react-query
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import tw, { styled, css } from 'twin.macro';
+const queryClient = new QueryClient();
 function App() {
   return (
-    <div>
-      <p>asd</p>
-    </div>
+    <CookiesProvider>
+      <QueryClientProvider lient={queryClient} contextSharing={true}>
+        <BrowserRouter className="App">
+          <div>
+            <Routes>
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </QueryClientProvider>
+    </CookiesProvider>
   );
 }
 
