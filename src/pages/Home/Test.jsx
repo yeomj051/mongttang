c04849432d9cea4dca70dd3916ac15e8e71fc79f
한상원 <wkas1921@naver.com>
2023-03-20 13:40:48 +0900

    [S08P22A308-166] FE/Feat: Add Countdown-Timer Function
    챌린지 남은시간 표시해주는 카운트다운 타이머 컴포넌트

--- src/pages/Home/Test.jsx
+++ src/pages/Home/Test.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import BookItem from 'components/common/BookItem';
+import ChallengeTimer from 'components/common/ChallengeTimer';
 
 function Test() {
-  return <BookItem />;
+  return (
+    <div>
+      {/* <BookItem /> */}
+      <ChallengeTimer />
+    </div>
+  );
 }
 
 export default Test;
