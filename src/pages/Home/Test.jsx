c58fe737dae45f4c0c8cac2786576bc4bc1f5f29
한상원 <wkas1921@naver.com>
2023-03-20 16:31:08 +0900

    [S08P22A308-170] FE/Feat: Add Bookshelf component
    책 선반 컴포넌트 추가
    챌린지 상세 정보표시 컴포넌트 추가

--- src/pages/Home/Test.jsx
+++ src/pages/Home/Test.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ChallengeTimer from 'components/common/ChallengeTimer';
+import BookShelf from 'components/common/BookShelf';
 
 function Test() {
   return (
     <div>
-      <ChallengeTimer />
+      {/* <ChallengeTimer /> */}
+      <BookShelf />
     </div>
   );
 }
