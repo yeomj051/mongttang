9dd33f8231803b786f642950c71864bdbda478de
한상원 <wkas1921@naver.com>
2023-03-20 11:06:24 +0900

    [S08P22A308-129] FE/Feat: Fix Book Component
    스타일 일부 수정

--- src/components/common/BookInfo.jsx
+++ src/components/common/BookInfo.jsx
@@ -1,19 +1,58 @@
 import React from 'react';
 import tw, { styled, css } from 'twin.macro';
 
-const Title = styled.div``;
-const Artist = styled.div``;
-const Likes = styled.div``;
-const Comments = styled.div``;
+import ProfileIcon from '../../assets/icons/UserIcon01.svg';
+import ReviewIcon from '../../assets/icons/ReviewIcon.svg';
+import LikeIcon from '../../assets/icons/LikeButtonFill.svg';
+
+const BookInfoWrapper = styled.div`
+  ${tw`pt-36`}
+`;
+
+const Title = styled.div`
+  ${tw`text-lg`}
+`;
+
+const SubInfoContainer = styled.div`
+  ${tw`flex flex-row flex-wrap w-36 text-[0.7rem] m-0.5 space-x-3`}
+`;
+
+const Artist = styled.div`
+  ${tw`flex flex-row`}
+`;
+
+const SubInfoWrapper = styled.div`
+  ${tw`flex flex-row space-x-1`}
+`;
+
+const Likes = styled.div`
+  ${tw`flex flex-row`}
+`;
+const Comments = styled.div`
+  ${tw`flex flex-row`}
+`;
 
 function BookInfo({ title, artist, likes, comments }) {
   return (
-    <div>
+    <BookInfoWrapper>
       <Title>제목</Title>
-      <Artist>작가명</Artist>
-      <Likes>좋아요수</Likes>
-      <Comments>댓글수</Comments>
-    </div>
+      <SubInfoContainer>
+        <Artist>
+          <img src={ProfileIcon} width={16} height={16} alt="" />
+          작가명
+        </Artist>
+        <SubInfoWrapper>
+          <Likes>
+            <img src={ReviewIcon} width={16} height={16} alt="" />
+            1234
+          </Likes>
+          <Comments>
+            <img src={LikeIcon} width={16} height={16} alt="" />
+            22
+          </Comments>
+        </SubInfoWrapper>
+      </SubInfoContainer>
+    </BookInfoWrapper>
   );
 }
 
--- src/components/common/BookItem.jsx
+++ src/components/common/BookItem.jsx
@@ -7,8 +7,21 @@ import tw, { styled, css } from 'twin.macro';
 
 import BookInfo from './BookInfo';
 
+const BookItemWrapper = styled.div`
+  ${tw``}
+`;
+
 const BookImage = styled.div`
-  ${tw`border-b w-40 h-40 transition duration-300 ease-in-out hover:opacity-40 hover:scale-110`}
+  ${tw`z-0 absolute transition w-36 h-48 rounded duration-300 ease-in-out hover:opacity-40 hover:scale-110`}
+  ${(props) =>
+    css`
+      background-image: url(${props.imgSrc});
+      background-size: cover;
+    `}
+`;
+
+const BookInfoWrapper = styled.div`
+  ${tw`z-50`}
 `;
 
 //props로 가져와야하는 정보 : 책 아이디(경로지정용), 책 이미지, 책 정보들(책 제목, 작가명, 댓글 수, 좋아요 수), 책 링크
@@ -29,25 +42,31 @@ function BookItem({
   const handleHover = (props) => {
     setTimeout(() => {
       setIsHover(props);
-    }, 150);
+    }, 100);
+    // setIsHover(props);
   };
 
   return (
-    <BookImage
-      onMouseOver={() => handleHover(true)}
-      onMouseOut={() => handleHover(false)}
-      onClick={() => navigate(`/books/${bookId}`)}
-    >
-      <img src={bookImg} alt="book" />
-      {isHover ? (
-        <BookInfo
-          title={bookTitle}
-          artist={artistNickname}
-          likes={numOfLike}
-          comments={numOfComment}
-        />
-      ) : null}
-    </BookImage>
+    <BookItemWrapper>
+      <BookImage
+        onMouseOver={() => handleHover(true)}
+        onMouseOut={() => handleHover(false)}
+        onClick={() => navigate(`/books/${bookId}`)}
+        imgSrc={bookImg}
+      >
+        {/* <img src={bookImg} alt="book" /> */}
+      </BookImage>
+      <BookInfoWrapper>
+        {isHover ? (
+          <BookInfo
+            title={bookTitle}
+            artist={artistNickname}
+            likes={numOfLike}
+            comments={numOfComment}
+          />
+        ) : null}
+      </BookInfoWrapper>
+    </BookItemWrapper>
   );
 }
 
