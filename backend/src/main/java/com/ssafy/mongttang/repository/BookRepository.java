57f68061ef24a8f9e53cda4f3af55aed771b93b9
여민지 <yeomj051@naver.com>
2023-03-21 10:23:10 +0900

    BE/Fix : Fix createInterest API bookID -> Book

--- backend/src/main/java/com/ssafy/mongttang/repository/BookRepository.java
+++ backend/src/main/java/com/ssafy/mongttang/repository/BookRepository.java
@@ -4,4 +4,5 @@ import com.ssafy.mongttang.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 public interface BookRepository extends JpaRepository<Book,Integer> {
 
+    Book findByBookId(int bookId);
 }
--- backend/src/main/java/com/ssafy/mongttang/repository/InterestBookRepository.java
+++ backend/src/main/java/com/ssafy/mongttang/repository/InterestBookRepository.java
@@ -1,14 +1,14 @@
 package com.ssafy.mongttang.repository;
 
+import com.ssafy.mongttang.entity.Book;
 import com.ssafy.mongttang.entity.InterestBook;
 import com.ssafy.mongttang.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.awt.print.Book;
 
 public interface InterestBookRepository extends JpaRepository<InterestBook, Integer> {
 
 
-    InterestBook findByInterestbookUserIdAndInterestbookBookId(User user, int bookId);
+    InterestBook findByInterestbookUserIdAndInterestbookBookId(User user, Book book);
 //    InterestBook findByUserIdAndBookId(User user, Book book);
 }
