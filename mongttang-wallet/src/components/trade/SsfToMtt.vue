f638148af3c6a14f479470b621f340936daa5ec9
khissup <khissup@naver.com>
2023-03-24 17:55:30 +0900

    S08P22A308-217 tradedevelop

---
+++ mongttang-wallet/src/components/trade/SsfToMtt.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+        <div class="row trade-choice">  
+            <div class="col-5">
+                <img src="../../assets/mttIcon.png" class="token-icon">몽땅코인 100개
+            </div>
+            <div class="col-3"></div>
+            <div class="col-4">
+                <button class="buy-button">1 SSF</button>
+            </div>
+        </div>
+        <div class="row trade-choice">  
+            <div class="col-5">
+                <img src="../../assets/mttIcon.png" class="token-icon">몽땅코인 200개
+            </div>
+            <div class="col-3"></div>
+            <div class="col-4">
+                <button class="buy-button">2 SSF</button>
+            </div>
+        </div>
+        <div class="row trade-choice">  
+            <div class="col-5">
+                <img src="../../assets/mttIcon.png" class="token-icon">몽땅코인 500개
+            </div>
+            <div class="col-3"></div>
+            <div class="col-4">
+                <button class="buy-button">5 SSF</button>
+            </div>
+        </div>
+        <div class="row trade-choice">  
+            <div class="col-5">
+                <img src="../../assets/mttIcon.png" class="token-icon">몽땅코인 1000개
+            </div>
+            <div class="col-3"></div>
+            <div class="col-4">
+                <button class="buy-button">10 SSF</button>
+            </div>
+        </div>
+        <div class="row trade-choice">  
+            <div class="col-5">
+                <img src="../../assets/mttIcon.png" class="token-icon">몽땅코인 10000개
+            </div>
+            <div class="col-3"></div>
+            <div class="col-4">
+                <button class="buy-button">100 SSF</button>
+            </div>
+        </div>
+    </div>
+</template>
+<style>
+.trade-choice {
+    
+}
+.buy-button {
+    margin-top: 5px;
+    color: #FFFFFF;
+    border-style: none;
+    background-color: #00D8FF;
+}
+</style>
