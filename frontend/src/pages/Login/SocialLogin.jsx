1408fd14419ef03d02b0c0a6dcb2653868403891
한상원 <swhan0428@gmail.com>
2023-03-27 10:55:42 +0900

    [S08P22A308-239] FE/Feat: Add Social Login API
    - 소셜로그인 redirect uri용 라우팅 추가
    - config 수정
    - 소셜로그인 API 연동

--- frontend/src/pages/Login/SocialLogin.jsx
+++ frontend/src/pages/Login/SocialLogin.jsx
@@ -9,43 +9,52 @@ import { userStore } from 'store/userStore';
 
 function SocialLogin() {
   const navigate = useNavigate();
-  const { setUserId, setUserNickname, setUserRole, setToken } = userStore(
-    (state) => state,
-  );
+  const { setUserId, setUserNickname, setUserImg, setUserRole, setToken } =
+    userStore((state) => state);
 
   const userId = new URL(window.location.href).searchParams.get('userId');
   const userNickname = new URL(window.location.href).searchParams.get(
-    'nickname',
+    'userNickname',
   );
   const accessToken = new URL(window.location.href).searchParams.get(
     'accessToken',
   );
-  const refreshToken = new URL(window.location.href).searchParams.get(
-    'refreshToken',
+
+  const profileImg = new URL(window.location.href).searchParams.get(
+    'profileImgURL',
   );
+  // const refreshToken = new URL(window.location.href).searchParams.get(
+  //   'refreshToken',
+  // );
 
   //로컬스토리지에 저장
   localStorage.setItem('userId', userId);
   localStorage.setItem('userNickname', userNickname);
   localStorage.setItem('accessToken', accessToken);
+  localStorage.setItem('userImg', profileImg);
+
   //refreshToken 저장 로직
   // localStorage.setItem('refreshToken', refreshToken);
-  setCookie('refreshToken', refreshToken, {
-    sameSite: 'strict',
-  });
+  // setCookie('refreshToken', refreshToken, {
+  //   sameSite: 'strict',
+  // });
 
   //전역상태 저장
   setUserId('userId', userId);
   setUserNickname('userNickname', userNickname);
+  setUserImg('userImg', profileImg);
   setUserRole('userRole', 'reader');
   setToken('accessToken', accessToken);
 
   //첫 로그인(회원가입)이라면 닉네임 설정 페이지로?
+  if (userNickname === '' || ' ') {
+    navigate('/myprofile/edit/nickname');
+  }
 
   //리다이렉트
   useEffect(() => {
     navigate('/home');
-  }, []);
+  });
 }
 
 export default SocialLogin;
