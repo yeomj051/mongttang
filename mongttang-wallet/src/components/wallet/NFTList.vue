abe1c1299b312d991a5a220ccb6079b2f42fa34a
이상훈 <akqjqdmltn@naver.com>
2023-03-27 17:29:07 +0900

    [S08P22A308-237] BC/Feat : Added total coin earned
    mongttangNFT revised, NFTList.vue

--- mongttang-wallet/src/components/wallet/NFTList.vue
+++ mongttang-wallet/src/components/wallet/NFTList.vue
@@ -2,10 +2,18 @@
     <h2>
       NFT
     </h2>
-    <div v-for="nft in nftList" :key="nft[0]">
-      <img src="http://dd93ub3tw0bvd.cloudfront.net/books/1/2/page0"/>
+    <div v-for="(nftId, idx) in nftIds" :key="nftId">
+      <img src="http://dd93ub3tw0bvd.cloudfront.net/books/1/2/page0" width="200"/>
       <div>
-        누적 MMT : {{ nft[1] }}        
+        <div>
+        토큰 : {{ nftId }}
+        </div>
+        <div>
+          현재 잔액 : {{ nftBalances[idx] }}
+        </div> 
+        <div>
+          총 수익 금액 : {{ nftTotalEarneds[idx] }}
+        </div>       
         <button type="button" class="btn btn-primary">출금하기</button>
       </div>
         
@@ -19,14 +27,19 @@
     name: 'NFTList',
     data(){
       return{
-        nftList : [],
+        nftIds : [],
+        nftBalances : [],
+        nftTotalEarneds : []
       }
     },
     created(){
-      getNTFList("0x10CA83EdC0E53589B22f23f4dEaAb53EEe70b1f9")
+      getNTFList()
       .then(res => {
         console.log(res);
-        this.nftList = res});
+        this.nftIds = res[0];
+        this.nftBalances = res[1];
+        this.nftTotalEarneds = res[2];        
+      });
     },
   }
   </script>
