0399f2474b0067034e776437406aee9b4444866b
한상원 <swhan0428@gmail.com>
2023-03-28 12:56:44 +0900

    [S08P22A308-266] FE/Feat: Fix Login Issue

--- frontend/src/pages/Login/SocialLogin.jsx
+++ frontend/src/pages/Login/SocialLogin.jsx
@@ -23,38 +23,27 @@ function SocialLogin() {
   const profileImg = new URL(window.location.href).searchParams.get(
     'profileImgURL',
   );
-  // const refreshToken = new URL(window.location.href).searchParams.get(
-  //   'refreshToken',
-  // );
-
-  //로컬스토리지에 저장
-  localStorage.setItem('userId', userId);
-  localStorage.setItem('userNickname', userNickname);
-  localStorage.setItem('accessToken', accessToken);
-  localStorage.setItem('userImg', profileImg);
-
-  //refreshToken 저장 로직
-  // localStorage.setItem('refreshToken', refreshToken);
-  // setCookie('refreshToken', refreshToken, {
-  //   sameSite: 'strict',
-  // });
-
-  //전역상태 저장
-  setUserId('userId', userId);
-  setUserNickname('userNickname', userNickname);
-  setUserImg('userImg', profileImg);
-  setUserRole('userRole', 'reader');
-  setToken('accessToken', accessToken);
-
   //첫 로그인(회원가입)이라면 닉네임 설정 페이지로?
-  if (userNickname === '' || ' ') {
-    navigate('/myprofile/edit/nickname');
-  }
 
   //리다이렉트
   useEffect(() => {
-    navigate('/home');
-  });
+    //로컬스토리지에 저장
+    localStorage.setItem('userId', userId);
+    localStorage.setItem('userNickname', userNickname);
+    localStorage.setItem('accessToken', accessToken);
+    localStorage.setItem('userImg', profileImg);
+    //전역상태 저장
+    setUserId('userId', userId);
+    setUserNickname('userNickname', userNickname);
+    setUserImg('userImg', profileImg);
+    setUserRole('userRole', 'reader');
+    setToken('accessToken', accessToken);
+    if (userNickname === '' || ' ') {
+      navigate('/myprofile/edit/nickname');
+    } else {
+      navigate('/home');
+    }
+  }, []);
 }
 
 export default SocialLogin;
