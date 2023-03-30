fa0bd8065bf1cd76ef060089ae7f9048bbb67322
nyh365 <12nyk34@naver.com>
2023-03-31 04:26:05 +0900

    [S08P22A308-275] BE/Fix: edit get this week challenge by total score

--- backend/src/main/java/com/ssafy/mongttang/controller/ChallengeController.java
+++ backend/src/main/java/com/ssafy/mongttang/controller/ChallengeController.java
@@ -3,6 +3,7 @@ package com.ssafy.mongttang.controller;
 import com.ssafy.mongttang.dto.ResponseChallengeBookInfoDto;
 import com.ssafy.mongttang.dto.ResponseDiscountBookDto;
 import com.ssafy.mongttang.dto.ResponseThisWeekChallengeDto;
+import com.ssafy.mongttang.dto.ResponseThisWeekChallengeNativeDto;
 import com.ssafy.mongttang.service.BookService;
 import com.ssafy.mongttang.service.ChallengeService;
 import io.swagger.annotations.ApiOperation;
@@ -29,6 +30,7 @@ public class ChallengeController {
     private static final String FAIL = "fail";
     private final ChallengeService challengeService;
     private final BookService bookService;
+
     @ApiOperation(value = "이번주 챌린지 조회", notes = "이번주 챌린지를 조회한다.", response = Map.class)
     @GetMapping
     public ResponseEntity<Map<String, Object>> getThisWeekChallenge(Principal principal) {
@@ -37,7 +39,7 @@ public class ChallengeController {
         if(principal != null){
             userId = Integer.valueOf(principal.getName());
         }
-        List<ResponseThisWeekChallengeDto> thisWeekChallenge = challengeService.getThisWeekChallenge(userId);
+        List<ResponseThisWeekChallengeNativeDto> thisWeekChallenge = challengeService.getThisWeekChallenge(userId);
         List<ResponseDiscountBookDto> discountBooks = bookService.getDiscountBooks();
         if(discountBooks != null && thisWeekChallenge != null){
             map.put(MESSAGE, SUCCESS);
@@ -49,6 +51,7 @@ public class ChallengeController {
             return new ResponseEntity<>(map, HttpStatus.BAD_REQUEST);
         }
     }
+
     @ApiOperation(value = "챌린지 상세 조회", notes = "챌린지를 상세 조회한다.", response = Map.class)
     @GetMapping("/{challengeId}")
     public ResponseEntity<Map<String, Object>> getChallengeDetail(@PathVariable @ApiParam(value = "상세 조회할 챌린지 아이디", example = "1") int challengeId
@@ -83,7 +86,7 @@ public class ChallengeController {
         if(principal != null){
             userId = Integer.valueOf(principal.getName());
         }
-        List<ResponseThisWeekChallengeDto> beforeChallenges = challengeService.getBeforeChallenge(userId);
+        List<ResponseThisWeekChallengeNativeDto> beforeChallenges = challengeService.getBeforeChallenge(userId);
         if(beforeChallenges != null){
             map.put(MESSAGE, SUCCESS);
             map.put("totalChallenges", beforeChallenges);
---
+++ backend/src/main/java/com/ssafy/mongttang/dto/ResponseThisWeekChallengeNativeDto.java
@@ -0,0 +1,30 @@
+package com.ssafy.mongttang.dto;
+
+import com.ssafy.mongttang.entity.Challenge;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+public class ResponseThisWeekChallengeNativeDto {
+    private int challengeId;
+    private String challengeTitle;
+    private String challengeContent;
+    private String challengeSummary;
+    private LocalDateTime challengeStartDate;
+    private LocalDateTime challengeEndDate;
+    private List<ResponseChallengeBookInfoNativeDto> bookList;
+
+    public ResponseThisWeekChallengeNativeDto(Challenge challenge, List<ResponseChallengeBookInfoNativeDto> bookList) {
+        this.challengeId = challenge.getChallengeId();
+        this.challengeTitle = challenge.getChallengeTitle();
+        this.challengeContent = challenge.getChallengeContent();
+        this.challengeSummary = challenge.getChallengeSummary();
+        this.challengeStartDate = challenge.getChallengeStartDate();
+        this.challengeEndDate = challenge.getChallengeEndDate();
+        this.bookList = bookList;
+    }
+}
