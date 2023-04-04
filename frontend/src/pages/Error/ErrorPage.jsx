e6ab0b2022326ef18f47205409d06b9af6f45f84
한상원 <swhan0428@gmail.com>
2023-04-04 23:37:43 +0900

    [S08P22A308-327] FE/Feat: Add Error Page in login case
    - 로그인 안한 유저의 에러페이지 분기 추가

--- frontend/src/pages/Error/ErrorPage.jsx
+++ frontend/src/pages/Error/ErrorPage.jsx
@@ -12,6 +12,10 @@ const ImgWrapper = styled.div`
   overflow: hidden;
 `;
 
+const BtnContainer = styled.div`
+  ${tw`flex space-x-1`}
+`;
+
 const TitleWrapper = styled.p`
   ${tw`text-2xl font-bold p-1`}
 `;
@@ -21,21 +25,53 @@ const ContentWrapper = styled.p`
 `;
 export default function ErrorPage() {
   const navigate = useNavigate();
-  console.log('도착했다');
+  const userId = localStorage.getItem('userId');
+
+  console.log(userId);
   return (
-    <BodyContainer>
-      <TitleWrapper>페이지를 찾을 수 없습니다.</TitleWrapper>
-      <ContentWrapper>
-        찾으려는 페이지의 주소가 잘못 입력되었거나,
-      </ContentWrapper>
-      <ContentWrapper>
-        주소의 변경 혹은 삭제로 인해 사용하실 수 없습니다.
-      </ContentWrapper>
-      <ContentWrapper>
-        입력하신 페이지의 주소가 정확한지 다시 한번 확인해 주세요.
-      </ContentWrapper>
-      <br />
-      <Button title="홈으로" buttonType="black" onClick={() => navigate('/')} />
-    </BodyContainer>
+    <div>
+      {userId ? (
+        <BodyContainer>
+          <TitleWrapper>페이지를 찾을 수 없습니다.</TitleWrapper>
+          <ContentWrapper>
+            찾으려는 페이지의 주소가 잘못 입력되었거나,
+          </ContentWrapper>
+          <ContentWrapper>
+            주소의 변경 혹은 삭제로 인해 사용하실 수 없습니다.
+          </ContentWrapper>
+          <ContentWrapper>
+            입력하신 페이지의 주소가 정확한지 다시 한번 확인해 주세요.
+          </ContentWrapper>
+          <br />
+          <Button
+            title="홈으로"
+            buttonType="black"
+            onClick={() => navigate('/')}
+          />
+        </BodyContainer>
+      ) : (
+        <BodyContainer>
+          <TitleWrapper>로그인한 사용자만 이용할 수 있습니다.</TitleWrapper>
+          <ContentWrapper>로그인하시고 다시 이용해주세요.</ContentWrapper>
+          <ContentWrapper>혹시 로그인하셨다면, 입력하신 정보가</ContentWrapper>
+          <ContentWrapper>
+            정확한지 다시 한번 확인해 주시기 바랍니다.
+          </ContentWrapper>
+          <br />
+          <BtnContainer>
+            <Button
+              title="로그인"
+              buttonType="black"
+              onClick={() => navigate('/login')}
+            />
+            <Button
+              title="홈으로"
+              buttonType="black"
+              onClick={() => navigate('/')}
+            />
+          </BtnContainer>
+        </BodyContainer>
+      )}
+    </div>
   );
 }
