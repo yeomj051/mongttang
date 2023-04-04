e2abe4d65d008463b1aa3059f791d4f39aa0f0b7
이상훈 <akqjqdmltn@naver.com>
2023-04-04 15:39:44 +0900

    dividing branch

---
+++ mongttang-wallet-front/src/api/backend.js
@@ -0,0 +1,30 @@
+import { createAPIInstance } from ".";
+
+const apiInstance = createAPIInstance();
+
+function getNFTList(address) {
+  return apiInstance.get(`/nft?address=${address}`);
+}
+
+function withdraw(privateKey, tokenId, amount) {
+  apiInstance.post("/nft/withdraw", {
+    privateKey: privateKey,
+    tokenId: tokenId,
+    amount: amount,
+  });
+}
+
+function buyMTT(privateKey, amount) {
+  apiInstance.post("/token/buy", {
+    privateKey: privateKey,
+    amount: amount,
+  });
+}
+
+function sellMTT(privateKey, amount) {
+  apiInstance.post("/token/sell", {
+    privateKey: privateKey,
+    amount: amount,
+  });
+}
+export { getNFTList, withdraw, buyMTT, sellMTT };
---
+++ mongttang-wallet-front/src/store/tradeModule.js
@@ -0,0 +1,14 @@
+export default {
+    state: {
+      // state properties
+    },
+    mutations: {
+      // mutation functions
+    },
+    actions: {
+      // action functions
+    },
+    getters: {
+      // getter functions
+    }
+  }
