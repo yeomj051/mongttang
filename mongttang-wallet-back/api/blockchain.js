fc3f7e80bd970371a0341116c3f279417a5ed5c4
LSH <akqjqdmltn@naver.com>
2023-04-07 02:36:00 +0900

    [S08P22A308-304] BC/Fix : fixing make nft function

--- mongttang-wallet-back/api/blockchain.js
+++ mongttang-wallet-back/api/blockchain.js
@@ -103,7 +103,7 @@ async function makeNFT(toAddress, tokenURI) {
         .on("receipt", (receipt) => {
           console.log(`Transaction confirmed: ${receipt.transactionHash}`);
           console.log(`Gas used: ${receipt.gasUsed}`);
-          console.log('receipt')
+          console.log(receipt);
         })
         .on("error", (error) => {
           console.error(`Transaction error: ${error}`);
