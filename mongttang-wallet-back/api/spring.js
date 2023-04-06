01fcb5dae1ef44b5769119339c2360b129fbbb66
이상훈 <akqjqdmltn@naver.com>
2023-04-06 16:45:20 +0900

    [S08P22A308-305] BC/Fix : testing

--- mongttang-wallet-back/api/spring.js
+++ mongttang-wallet-back/api/spring.js
@@ -3,10 +3,14 @@ import { createAPIInstance } from "./index.js";
 const apiInstance = createAPIInstance();
 
 async function postNFTID(bookId, nftId) {
-  apiInstance.post("/book/token", {
-    bookId: bookId,
-    nftId: nftId,
-  });
+  apiInstance
+    .post("/book/token", {
+      bookId: bookId,
+      nftId: nftId,
+    })
+    .catch((err) => {
+      console.log("err2");
+    });
 }
 
 export { postNFTID };
