2f026625baba200df678d5fa3f81360e57b922a9
LSH <akqjqdmltn@naver.com>
2023-04-06 23:17:10 +0900

    organizing file structure

--- mongttang-wallet-front/src/main.js
+++ mongttang-wallet-front/src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+import store from './store/index.env'
 import BootstrapVue3 from 'bootstrap-vue-3'
 
 // Optional, since every component import their Bootstrap functionality
